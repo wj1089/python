1c1c8d9ed148312d5da58a8a182050b8c8604cc4
wj1089 <wj1089@naver.com>
2020-07-03 10:39:18 +0900

    20.07.03
    **5번작업

--- mining.py
+++ mining.py
@@ -5,10 +5,13 @@ import re
 import nltk
 from nltk.tokenize import word_tokenize
 from konlpy.tag import Okt
-import pandas as pd
 from nltk import FreqDist
+
+import pandas as pd
+import re
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+
 
 
 
@@ -39,93 +42,114 @@ class Entity:
 
 class Service:
     def __init__(self):
-        self.text = []
-        self.token = []
-        self.okt()
+        self.texts = []
+        self.tokens = []
+        self.noun_tokens = []
+        self.okt = Okt()
         self.stopwords = []
         self.freqtxt = []
 
-    def extrect_token(self, payload):
-        print('>> corpus에서 token 추출')
+    def extract_texts(self, payload):
+        print('1. corpus 에서 token 추출')
         filename = payload.context + payload.fname
         with open(filename, 'r', encoding='utf-8') as f:
-            self.text = f.read()
+            self.texts = f.read()
         print(f'1단계 결과물 : {self.texts[:300]}')
 
-    def extrect_hanguel(self):
-        print('2. corpus에서 한글 추출')
-        texts = self.texts.replace('\n',' |')
-        tokenizer = re.compile(r'[^ㄱ-힣]') 
-        # ^ 는 not과 start 두가지 개념이 있음
-        # [^] 는 not, ^[]은 start 의ㅣ미로 표현됨. 
-        self.texts = tokenizer.sub('',texts)
-        # 한글이 아닌 것은 '' 처리ㅇ해서 한글만 남겨라
+    def tokenize(self):
+        print('2. corpus 에서 한글 추출')
+        texts = self.texts.replace('\n',' ')
+        tokenizer = re.compile(r'[^ㄱ-힣]')
+        # ^ 는 not 과 start 두가지 개념이 있음
+        # [^] 은 not, ^[] 은 start 의미로 표현됨
+        self.texts = tokenizer.sub(' ', texts)
+        # 한글이 아닌 것은 ' ' 처리해서 한글과 띄어쓰기 남겨라
+        print(f'2단계 결과물 : {self.texts[:300]}')
 
 
     def conversion_token(self):
-        print('3. corpus에서 token 추출')
+        print('3. 한글 token 변환')
+        self.tokens = word_tokenize(self.texts)
+        print(f'3단계 결과물 : {self.tokens[:300]}')
 
-    def compount_noun(self):
-        print('4. 복합명사화')
+    def compound_noun(self):
+        print('4. 복합명사화 ')
+        arr_ = []
+        for token in self.tokens:
+            token_pos = self.okt.pos(token)
+            _ = [txt_tags[0] for txt_tags in token_pos
+                 if txt_tags[1] == 'Noun']
+            if len("".join(_)) > 1:
+                arr_.append("".join(_))
+        self.noun_tokens = " ".join(arr_)
+        print(f'4단계 결과물 : {self.noun_tokens[:300]}')
 
-    def extrect_stopword(self):
+    def extract_stopword(self, payload):
         print('5. 노이즈 코퍼스에서 토큰 추출')
+        filename = payload.context + payload.fname
+        with open(filename, 'r', encoding='utf-8') as f:
+            self.stopwords = f.read()
+        print(f'5단계 결과물 : {self.texts[:300]}')
+
 
     def filtering_text_with_stopword(self):
         print('6. 노이즈 필터링 후 시그널 추출')
+        self.noun_tokens = word_tokenize(self.noun_tokens)
+        self.noun_tokens = [text for text in self.noun_tokens
+                            if text not in self.stopwords]
+
 
     def frequent_text(self):
         print('7. 시그널 중에 사용빈도 정렬')
+        self.freqtxt = pd.Series(dict(FreqDist(self.noun_tokens))).sort_values(ascending=False)
+        print(f'{self.freqtxt[:10]}')
 
-    def wordcloud(self):
-        print('8. 시각화')
 
+    def wordcloud(self, payload):
+            print('8. 시각화')
+            fname = payload.context + payload.fname
+            wcloud = WordCloud(fname, relative_scaling=0.2, background_color='white',colormap='Blues') \
+                .generate(" ".join(self.noun_tokens))
+            plt.figure(figsize=(12, 12))
+            plt.imshow(wcloud, interpolation='bilinear')
+            plt.axis('off')
+            plt.show()
 
 class Controller:
     def __init__(self):
         pass
-    
+
     def download_dictionary(self):
         nltk.download('all')
 
     def data_analysis(self):
         entity = Entity()
         service = Service()
-        service.extrect_token()
-        service.extrect_hanguel()
+        entity.context = './data/'
+        entity.fname = 'kr-Report_2018.txt'
+        service.extract_texts(entity)
+        service.tokenize()
         service.conversion_token()
-        service.compount_noun()
-        service.extrect_stopword()
+        service.compound_noun()
+        entity.fname = 'stopwords.txt'
+        service.extract_stopword(entity)
         service.filtering_text_with_stopword()
         service.frequent_text()
-        service.wordcloud()
-
-def print_nemu():        
-    print('0. Exit')
-    print('1. 사전 다운로드')
+        entity.fname = 'D2Coding.ttf'
+        service.wordcloud(entity)
 
 
+def print_menu():
+    print('0. Exit\n')
+    print('1. 사전 다운로드\n')
+    return input('메뉴 선택\n')
 
-    return  input('메뉴 선택\n')
-    
 app = Controller()
 while 1:
-    menu = print_nemu()
+    menu = print_menu()
     if menu == '1':
         app.download_dictionary()
     if menu == '2':
         app.data_analysis()
     if menu == '0':
         break
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
